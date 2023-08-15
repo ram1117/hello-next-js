b953bdc225ea4d59dcc7c0ead64b4efa9077bb1c
ram1117 <ramkumark1@outlook.com>
2023-08-15 14:07:15 +0530

    add video to the page

--- src/app/agreement/page.tsx
+++ src/app/agreement/page.tsx
@@ -1,4 +1,18 @@
 function Page() {
-  return <h1 className="text-2xl">Terms and Conditions</h1>;
+
+  return (
+    <div className="w-1/2 h1/2 my-16">
+      <video
+        width="100%"
+        height="100%"
+        controls
+      >
+        <source
+          src="https://drive.google.com/uc?id=1IxVST9uxX-AbfB5iORON9k3sAGlb6IpA"
+          type="video/mp4"
+        />
+      </video>
+    </div>
+  );
 }
 export default Page;
