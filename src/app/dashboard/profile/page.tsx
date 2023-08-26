09045310e73fe4ec71648599b9f5e74adb169215
ram1117 <ramkumark1@outlook.com>
2023-08-26 15:51:14 +0530

    test progressbar

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 
 function Page() {
+  const [loadingProgress, setLoadingProgress] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    console.log(isLoading)
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    if (isLoading) {
+      setLoadingProgress(75);
+    }
+    if (!isLoading) {
+      setLoadingProgress(100);
+    }
+  }, [isLoading]);
   return (
     <div>
       <h2 className="text-3xl">Profile sample Page</h2>
+      <div className="h-[20px] w-[20rem] bg-blue-200 rounded-full">
+        <div
+          className={`h-full bg-blue-700 rounded-full animate-pulse`}
+          style={{
+            width: `${loadingProgress}%`,
+            transition: 'width 2s ease-in-out',
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,38 @@
-import React from 'react';
+"use client"
 
+import React from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
+
+const minusIcon = '-';
+const plusIcon = '+';
+
+function Accordion({ title, content }) {
+  const [expanded, setExpanded] = useState(false);
+  const toggleExpanded = () => setExpanded((current) => !current);
+
+  return (
+    <div
+      className="my-2 sm:my-4 md:my-6 shadow-sm cursor-pointer bg-white"
+      onClick={toggleExpanded}
+    >
+      <div className="px-6 text-left items-center h-20 select-none flex justify-between flex-row">
+        <h5 className="flex-1">{title}</h5>
+        <div className="flex-none pl-2">{expanded ? minusIcon : plusIcon}</div>
+      </div>
+      <div
+        className={`px-6 pt-0 overflow-hidden transition-[max-height] duration-500 ease-in ${
+          expanded ? 'max-h-40' : 'max-h-0'
+        }`}
+      >
+        <p className="pb-4 text-left">{content}</p>
+      </div>
+    </div>
+  );
+}
+
+const lorem =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 export default function Home() {
   return (
@@ -41,6 +73,13 @@ export default function Home() {
         >
           Page 3
         </Link>
+      </div>
+      <div className="py-16 md:py-20 lg:py-24 px-4 bg-black">
+        <section className="max-w-6xl mx-auto text-center">
+          <Accordion title="Accordion #1" content={lorem} />
+          <Accordion title="Accordion #2" content={lorem} />
+          <Accordion title="Accordion #3" content={lorem} />
+        </section>
       </div>
     </main>
   );
